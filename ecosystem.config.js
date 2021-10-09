f1cef6b6ae05f362394d5ce5c61eb10e82eec1ef
seunexplicit <seunxplicit@gmail.com>
2021-10-09 15:19:56 +0100

    100920210230

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,6 @@
 module.exports = {
      apps: [{
+          name: "development",
           script: "src/index.ts",
           /*script:"./node_modules/.bin/ts-node -r ./node_modules/tsconfig-paths/register src/index.ts",*/
           watch: true,
@@ -7,22 +8,31 @@ module.exports = {
           interpreter_args: '--require ./node_modules/ts-node/register --require .//node_modules/tsconfig-paths/register',
           env: {
                "NODE_ENV": "development"
-          },
-          env_production:{
-               "NODE_ENV": "production"
           }
-  }],
+     },
+     {
+          name: "production",
+          script: "dist/index.js",
+          /*script:"./node_modules/.bin/ts-node -r ./node_modules/tsconfig-paths/register src/index.ts",*/
+          watch: ['/dist'],
+          /*interpreter: 'node',
+          interpreter_args: '--require ./node_modules/ts-node/register --require .//node_modules/tsconfig-paths/register',*/
+          env: {
+               "NODE_ENV": "development"
+          }
+     }
+     ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/main',
-            repo: 'https://github.com/seunexplicit',
-            path: '/lotim-minicommerce',
-      'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --ext ts,json',
-      'pre-setup': ''
-       }
-  }
+     deploy: {
+          production: {
+               user: 'SSH_USERNAME',
+               host: 'SSH_HOSTMACHINE',
+               ref: 'origin/main',
+               repo: 'https://github.com/seunexplicit',
+               path: '/lotim-minicommerce',
+               'pre-deploy-local': '',
+               'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --ext ts,json',
+               'pre-setup': ''
+          }
+     }
 };
